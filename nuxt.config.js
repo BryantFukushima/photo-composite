ea0092ab650595081c568614b69c674c648ab94d
Bryant Fukushima <bmfukushima94@gmail.com>
2021-10-14 02:05:55 -0700

    creates(styles/): add global styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,29 +1,26 @@
 export default {
-  target: 'static',
+  target: "static",
   head: {
-    title: 'testsite',
+    title: "testsite",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   css: [
+    "@/styles/base/reset.sass",
+    "@/styles/base/typography.sass",
+    "@/styles/base/static.sass"
   ],
-  plugins: [
-  ],
+  plugins: [],
   components: true,
-  buildModules: [
-  ],
-  modules: [
-  ],
-  build: {
-  }
-}
+  buildModules: [],
+  modules: [],
+  build: {}
+};
