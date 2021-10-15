3271c34a4ec0a0e6ffe76e3b182400add47600ab
Bryant Fukushima <bmfukushima94@gmail.com>
2021-10-15 03:52:41 -0700

    chore(): run build

---
+++ dist/_nuxt/static/1634295143/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1634295143",layout:"default",error:a,state:{modalData:a,"mutation-types":{},storeActions:{},storeMutations:{}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
