f6cb2e08398f339f49ea66f608af77f2c80f940d
Bryant Fukushima <bmfukushima94@gmail.com>
2021-10-15 03:30:48 -0700

    updates(Modal): remove html2canvas

--- components/Modal/index.vue
+++ components/Modal/index.vue
@@ -9,27 +9,6 @@ export default {
       return this.$store.state.modalData;
     }
   },
-  watch: {
-    modalActive(newState, oldState) {
-      if (newState) {
-        this.$nextTick(() => {
-          html2canvas(this.$refs.comp, {
-            allowTaint: true,
-            letterRendering: true
-          }).then(canvas => {
-            const downloadBtn = this.$refs.download;
-            const canvasElem = this.$refs.canvas;
-            canvasElem.appendChild(canvas);
-            downloadBtn.download = "composite.jpg";
-            downloadBtn.href = canvasElem.children[0].toDataURL("image/jpg");
-          });
-        });
-      }
-    }
-  },
-  mounted() {
-    console.log("modal");
-  },
   methods: {
     closeModal() {
       this.$store.commit("MODAL_DATA", null);
